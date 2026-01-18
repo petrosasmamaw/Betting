085c414ef78db2778f8c4b0d122da3ee7b519752
Petros Asmamaw <petrosasmamaw2@gmail.com>
2026-01-18 20:35:25 +0300

    add corsoption

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,8 @@ const corsOptions = {
 	origin: [
 		'http://localhost:5173',
 		'http://localhost:5174',
-		'http://localhost:5175',
+		'https://secret-bet-client.vercel.app/',
+		'https://secret-bet-admin.vercel.app/',
 	],
 	credentials: true,
 };
